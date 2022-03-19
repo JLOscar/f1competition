f6ffbf163df04b5f2aa42d8b9f11b8081f738a67
JLOscar <oscar.jonassonlundberg@outlook.com>
2022-03-19 13:16:09 +0100

    Created working driver standings route

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import {
+  getCurrentDriverFinishedStatus,
+  getCurrentDriverStandings,
+} from "./routes/getCurrentStandings";
 
 const app = express();
-const PORT = 8080;
+const PORT = 8000;
 
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+export const apiBaseUrl = "http://ergast.com/api/f1";
+
+app.get("/driver-standings", async (req, res) => {
+  //TODO: Remove and replace with real current round
+  const round = 2;
+  const driverStandings = await getCurrentDriverStandings(round);
+  res.json(driverStandings);
+});
+
+app.get("/finished-status", async (req, res) => {
+  //TODO: Not finished :)
+  const driverStandings = await getCurrentDriverFinishedStatus();
+  res.json(driverStandings);
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
